164e8faedfb3edfc665dc0274039d0ed40a51010
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-12-09 13:32:33 +0530

    added

---
+++ script.js
@@ -0,0 +1,74 @@
+// alert("Welcome to all")
+
+// a='deepak'
+// console.log("hellow",a);
+
+// var a='heloo'
+// var a=20
+
+// console.log(a);
+
+// let a=20
+// console.log(a);
+
+// let a={name:'dpk',age:21}
+// let b=true
+// let c=(1,2,3)
+// let d=[1,2,3,4,]
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+
+// ARITHAMATIC OPERATOR
+
+// console.log(10+5);
+
+// +,-,/,%,**,++,--
+// let a=5
+// let b=5
+// ++a
+// --b
+// a++
+// b--
+
+// console.log(a);
+// console.log(b);
+
+// ASSIGNMENT OPERTORS
+// +=,-=,*=,/=,%=,**=
+
+// let a=10
+// a*=10
+// console.log(a);
+let a=10
+let b=20
+
+// COMPARISON 
+// ==,!=,<,>,<=,>=,===,!==
+
+console.log(a===b);
+console.log(a!==b);
+
+// LOGICAL
+// &&
+// ||
+
+console.log(a==b && a==10);
+console.log(a==b || a==10);
+ 
+
+
+
+
+
+
+
